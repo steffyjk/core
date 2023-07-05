9f8961e862a9f18758bb13c332ffa156508e225f
steffyjk <steffy.jk2018@gmail.com>
2023-07-05 15:08:01 +0530

    Feat: all tables added into admin panel --> registered

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import AboutMe, ExperienceTechnology, ExperienceCategory
+from .models import AboutMe, ExperienceTechnology, ExperienceCategory,Service, SubService, WorkCategory, MyWork, Myinfo, ContactMe
 
 # Register your models here.
 admin.site.register(AboutMe)
 admin.site.register(ExperienceTechnology)
 admin.site.register(ExperienceCategory)
+admin.site.register(Service)
+admin.site.register(SubService)
+admin.site.register(WorkCategory)
+admin.site.register(Myinfo)
+admin.site.register(MyWork)
+admin.site.register(ContactMe)
