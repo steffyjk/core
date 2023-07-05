0056e7389a4f65c2ce95d8c204e1a15ee44fd0e0
steffyjk <steffy.jk2018@gmail.com>
2023-07-05 15:11:03 +0530

    Feat: categories of experience is passed as context for main index.html --> worked on ExperienceTechnology and ExperienceCategory

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from .models import AboutMe
+from .models import AboutMe, ExperienceTechnology, ExperienceCategory
 
 
 def home(request):
     about_me = AboutMe.objects.first()
+    exp_techs = ExperienceTechnology.objects.all()
+    exp_cats = ExperienceCategory.objects.all()
+    experience = {f"{i.name}": [] for i in exp_cats}
+    for exp_cat in exp_cats:
+
+        for exp_tech in exp_techs:
+            if exp_tech.exp_cat_id.id == exp_cat.id:
+                experience[f"{exp_cat.name}"].append({
+                    "tech_name": exp_tech.tech_name,
+                    "tech_qualification": exp_tech.tech_qualification
+                })
+    categories = ExperienceCategory.objects.all()
     context = {
-        "about_me": about_me
+        "about_me": about_me,
+        "categories": categories,
     }
     return render(request, 'index.html', context=context)
