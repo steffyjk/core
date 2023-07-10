f0bbacc009072761206ff688f71fb9f468b40a2b
steffyjk <steffy.jk2018@gmail.com>
2023-07-10 18:38:11 +0530

    Feat: added str methods for all models in models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -13,9 +13,15 @@ class AboutMe(models.Model):
     total_projects = models.IntegerField()
     description = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.description}"
+
 
 class ExperienceCategory(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class ExperienceTechnology(models.Model):
@@ -23,19 +29,31 @@ class ExperienceTechnology(models.Model):
     tech_name = models.CharField(max_length=255)
     tech_qualification = models.CharField(max_length=45, choices=QUALIFICATION_CHOICES)
 
+    def __str__(self):
+        return f"{self.tech_name}"
+
 
 class Service(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title}"
 
 
 class SubService(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     sub_service = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.sub_service}"
+
 
 class WorkCategory(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class MyWork(models.Model):
@@ -44,14 +62,23 @@ class MyWork(models.Model):
     image = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.description}"
+
 
 class Myinfo(models.Model):
     plateform_name = models.CharField(max_length=255)
     plateform_id = models.CharField(max_length=255)
     plateform_link = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"name: {self.plateform_name} | id: {self.plateform_id}"
+
 
 class ContactMe(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     description = models.CharField(max_length=400)
+
+    def __str__(self):
+        return f"name: {self.name} | email: {self.email} | desc: {self.description}"
