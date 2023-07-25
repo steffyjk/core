b2f6cea002943be0761542ce1b18e9bfcb5eea70
steffyjk <steffy.jk2018@gmail.com>
2023-07-25 18:38:09 +0530

    Feat: changed static dir name to statics

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 STATIC_URL = "/statics/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "statisc")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "statics"),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
